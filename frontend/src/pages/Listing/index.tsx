ea5a4943bde50090ec68597955d7c52aabb9fed5
Alexsandro Castro <sandrosilva9029@gmail.com>
2022-03-11 16:28:33 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,14 @@
 import Pagination from "components/Pagination"
 import MovieCard from "components/MovieCard";
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
+
+//forma errada
+axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(response => {
+        console.log(response.data);
+    })
+
 
 
 function Listing() {
